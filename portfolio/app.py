ed20921b9a72b6cd7fef09974402ec63e70f2b2e
ajversaily <ajrasaily08@gmail.com>
2026-05-11 21:00:59 -0400

    Add files via upload

---
+++ portfolio/app.py
@@ -0,0 +1,20 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def load_projects():
+    with open('data/projects.json') as f:
+        return json.load(f)['projects']
+
+@app.route('/')
+def index():
+    projects = load_projects()
+    return render_template('index.html', projects=projects)
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
